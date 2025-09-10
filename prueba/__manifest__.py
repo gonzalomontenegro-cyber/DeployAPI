007d7842b5d19de146893d7f1897c4a01bea98ab
gonzalomontenegro-cyber <gonzalo.montenegro@sellside.cl>
2025-09-10 22:16:41 +0000

    Correcciones en el manifest

--- prueba/__manifest__.py
+++ prueba/__manifest__.py
@@ -14,7 +14,7 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Ejemplo',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -30,5 +30,8 @@ Long description of module's purpose
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
 
